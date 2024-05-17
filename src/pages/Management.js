bd100b18f6bfa8b01b7155bbbcdbb9e14b0518a8
ChristianViracocha44 <sistemas.ti4@sedemi.com>
2024-05-16 23:33:54 -0500

    Eliminar archivos sin uso

--- src/pages/Management.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Management=()=>{
-
-    return(
-        <div>
-            <p>Management</p>
-        </div>
-    )}
--- src/router/LibraryRouter.js
+++ src/router/LibraryRouter.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import {About} from '../pages/About';
 import { Catalog } from "../pages/Catalog";
 import { Home } from "../pages/Home";
-import { Management } from "../pages/Management";
 import {NotFound} from "../pages/NotFound";
 import { Store } from "../pages/Store";
 
@@ -15,7 +14,6 @@ export const LibraryRouter = () => {
         <Route path="/catalog" element={<Catalog/>}/>
         <Route path="/home" element={<Home/>}/>
         <Route path="/" element={<Home/>}/>
-        <Route path="/management" element={<Management/>}/>
         <Route path="/store/:bookid" element={<Store/>}/>
         <Route path="/store/" element={<Store/>}/>
         <Route path="*" element={<NotFound/>} />
